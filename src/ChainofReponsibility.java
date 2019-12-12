46a27bee57f868f46e9ec042f3fec9b4a3e6d3bd
PeiYauWeng <pweng@cpp.edu>
2019-12-11 17:05:57 -0800

    modified Employee class

--- src/ChainofReponsibility.java
+++ src/ChainofReponsibility.java
@@ -17,10 +17,6 @@ public class ChainofReponsibility {
 	
 	public static void main(String args[]) {
 		Employee chainEmployee = doChaining();
-		
 		chainEmployee.doWork(Employee.PART_COLLECTOR, "all parts have been gathered");
-		chainEmployee.doWork(Employee.ASSEMBLER, "all parts have been put together");
-		chainEmployee.doWork(Employee.WELDER, "all parts have been welded together");
-		chainEmployee.doWork(Employee.PAINTER, "car complete");
 	}
 }
--- src/Employee.java
+++ src/Employee.java
@@ -14,12 +14,23 @@ public abstract class Employee {
 	
 	public void doWork(int authorityLevel, String message) {
 		if(this.authorityLevel <= authorityLevel) {
+			message = generateMsg(this.authorityLevel, message);
 			write(message);
 		}
 		
 		if(nextEmployee != null) {
 			nextEmployee.doWork(authorityLevel, message);
 		}
+	}
+	
+	private String generateMsg(int level, String msg) {
+		if (level == 3)
+			return "all parts have been put together";
+		if (level == 2)
+			return "all parts have been welded together";
+		if (level == 1)
+			return "car complete";
+		return msg;
 	}
 	
 	protected abstract void write(String message);
